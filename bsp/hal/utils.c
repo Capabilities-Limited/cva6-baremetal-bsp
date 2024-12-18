50ba4fc62928928880d4fe534501ef745ac50d11
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2024-12-18 22:21:33 +0000

    Use existing read_csr function

--- bsp/hal/utils.c
+++ bsp/hal/utils.c
@@ -1,17 +1,15 @@
 // Copyright 2024 Capabilities Limited
 
 #include "time.h"
+#include "encoding.h"
 
 int usleep(useconds_t wait)
 {
-  register uint64_t start_cycles;
-  // While the asm does not clobber memory, avoid reordering by indicating it does
-  __asm__ __volatile__ ("csrr %0, mcycle" : "=r" (start_cycles) : : "memory");
+  register uint64_t start_cycles = read_csr(mcycle);
   uint64_t CYCLES_PER_USEC = 50; // 50MHz
   uint64_t target_cycles = wait * CYCLES_PER_USEC;
   while (1) {
-    register uint64_t cur_cycles;
-    __asm__ __volatile__ ("csrr %0, mcycle" : "=r" (cur_cycles) : : "memory");
+    register uint64_t cur_cycles = read_csr(mcycle);
     if (cur_cycles - start_cycles >= target_cycles) {
       break;
     }
